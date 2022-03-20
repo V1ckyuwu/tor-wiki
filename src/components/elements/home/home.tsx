cc82fcbfed4a316cbe5707443bb7416236a6cef1
Victoriano Pedrajas <36993779+V1ckyuwu@users.noreply.github.com>
2022-03-20 08:04:12 +0100

    fixing errors

--- src/components/elements/home/home.tsx
+++ src/components/elements/home/home.tsx
@@ -6,7 +6,7 @@ import { AboutUs } from './aboutUs';
 import { AdventureStart } from './adventureStart';
 import { ChooseYourStory } from './chooseStory';
 import { JoinUs } from './joinUs';
-import { ModeratorCarousel } from './moderatorCarousel';
+// import { ModeratorCarousel } from './moderatorCarousel';
 import { HomeStats } from './stats';
 
 export const Home: Component = () => {
